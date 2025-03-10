ed80426db53b19e36ca4ada183bdcfd8fd2e9659
neithiri <popokakapo76xxx@gmail.com>
2025-03-10 22:41:04 +0700

    Cписок технологий JSON
    Добавил список технологий, по которым будут вопрос. Как я понял, я должен был сделать ApiController, и я его сделал, используя наследование от ControllerBase вместо Controller, потому что мы используем апи и у нас нет вью

---
+++ TGBot/TGBot/Controllers/TechnologiesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TGBot.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TechnologiesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = new TechnologiesClass
+            {
+                Technologies = new List<string> { "python", "java" }
+            };
+
+            return Ok(data);
+        }
+    }
+}
---
+++ TGBot/TGBot/TechnologiesClass.cs
@@ -0,0 +1,7 @@
+﻿namespace TGBot
+{
+    public class TechnologiesClass
+    {
+        public List<string> Technologies { get; set; }
+    }
+}
