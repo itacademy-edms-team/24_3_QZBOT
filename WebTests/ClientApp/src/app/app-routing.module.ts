290c3782ce02c467c030a1b40fb5b3ef0f19cd35
neithiri <popokakapo76xxx@gmail.com>
2025-10-17 15:36:17 +0700

    remove trash

--- WebTests/ClientApp/src/app/app-routing.module.ts
+++ WebTests/ClientApp/src/app/app-routing.module.ts
@@ -1,11 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TestComponent } from './tests/tests.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: 'todo', component: TodoListComponent },
   { path: 'tests/:name', component: TestComponent },
 ];
 
--- WebTests/ClientApp/src/app/app.module.ts
+++ WebTests/ClientApp/src/app/app.module.ts
@@ -7,27 +7,17 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { TodoListComponent } from './todo-list/todo-list.component';
 import { AppRoutingModule } from './app-routing.module';
-import { PythonTestComponent } from './pages/python-test/python-test.component';
-import { CsharpTestComponent } from './pages/csharp-test/csharp-test.component';
-import { SqlTestComponent } from './pages/sql-test/sql-test.component';
 import { TestComponent } from './tests/tests.component';
+import { TestsListComponent } from './tests-list/tests-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
-    TodoListComponent,
-    CsharpTestComponent,
-    PythonTestComponent,
-    SqlTestComponent,
-    TestComponent
+    TestComponent,
+    TestsListComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -35,12 +25,8 @@ import { TestComponent } from './tests/tests.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'python-test', component: PythonTestComponent },
-      { path: 'csharp-test', component: CsharpTestComponent },
-      { path: 'sql-test', component: SqlTestComponent },
-      { path: 'tests/:name', component: TestComponent }
+      { path: 'tests/:name', component: TestComponent },
+      { path: 'tests', component: TestsListComponent }
     ]),
     AppRoutingModule
   ],
