6470218102c998268156e60bf9bc0f4aec0cc970
neithiri <popokakapo76xxx@gmail.com>
2026-01-14 22:53:35 +0700

    Create TestReadDto.cs

---
+++ WebTests/DTOs/TestReadDto.cs
@@ -0,0 +1,15 @@
+﻿using System.Data.Entity.Core.Mapping;
+
+namespace WebTests.DTOs
+{
+    public class TestReadDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string CreatorId { get; set; }
+        public List<string> Types { get; set; }
+        public List<QuestionDto> Questions { get; set; }
+        public bool Published { get; set; }
+        public int MinimumSuccessPercent { get; set; }
+    }
+}
