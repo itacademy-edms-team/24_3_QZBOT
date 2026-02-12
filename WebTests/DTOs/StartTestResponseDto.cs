6e300c1b070329247e54855bf835ce9cefc402a2
neithiri <popokakapo76xxx@gmail.com>
2026-02-12 22:17:30 +0700

    add dtos

---
+++ WebTests/DTOs/StartTestResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace WebTests.DTOs
+{
+    public class StartTestResponseDto
+    {
+        public int UserTestId { get; set; }
+        public QuestionDto? NextQuestion { get; set; }
+        public List<int> AnsweredQuestionIds { get; set; }
+    }
+}
---
+++ WebTests/DTOs/UserAnswerDto.cs
@@ -0,0 +1,8 @@
+﻿namespace WebTests.DTOs
+{
+    public class UserAnswerDto
+    {
+        public int QuestionId { get; set; }
+        public List<int> SelectedOptionIds { get; set; }
+    }
+}
