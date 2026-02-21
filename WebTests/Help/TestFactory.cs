f959a51b958a9b94a2446af0ace7d1c376956b25
neithiri <popokakapo76xxx@gmail.com>
2026-02-21 20:33:13 +0700

    добавил вспомогательный метод
    для подсчета балла за каждый ответ

---
+++ WebTests/Help/TestFactory.cs
@@ -0,0 +1,114 @@
+﻿using WebTests.Data;
+using WebTests.DTOs;
+using WebTests.Models;
+
+namespace WebTests.TestFactory
+{
+    public static class FromDto
+    {
+        public static Test Create(TestDto dto, AppDbContext context)
+        {
+            var test = new Test();
+            test.Title = dto.Title;
+            test.CreatedDate = DateTime.UtcNow;
+
+            if (dto.Published)
+                test.Published = true;
+            else
+                test.Published = false;
+
+
+            var types = context.TestTypes
+                .Where(t => dto.Types.Contains(t.Name))
+                .ToList();
+
+            test.Types = types;
+
+            test.MinSuccessPercent = dto.MinimumSuccessPercent;
+
+
+            foreach (var question in dto.Questions)
+            {
+                if (question.Options == null)
+                {
+                    continue;
+                }
+
+                var quest = new Question()
+                {
+                    Text = question.Text,
+                    IsMultiple = question.isMultiple
+                };
+
+                if (quest.Options == null)
+                {
+                    quest.Options = new List<AnswerOption>();
+                }
+
+                foreach (var option in question.Options)
+                {
+                    var opt = new AnswerOption()
+                    {
+                        Text = option.Text,
+                        IsCorrect = option.IsCorrect
+                    };
+
+                    quest.Options.Add(opt);
+                }
+
+                if (test.Questions == null)
+                {
+                    test.Questions = new List<Question>();
+                }
+
+                test.Questions.Add(quest);
+            }
+
+            return test;
+        }
+
+        public static void Update(Test test, TestDto dto, AppDbContext context)
+        {
+            test.Title = dto.Title;
+
+            bool wasPublished = test.Published;
+            test.Published = dto.Published;
+
+            if (!wasPublished && dto.Published)
+                test.PublishDate = DateTime.UtcNow;
+
+            test.EditTime = DateTime.UtcNow;
+
+
+
+            test.Types.Clear();
+
+            var types = context.TestTypes
+                .Where(t => dto.Types.Contains(t.Name))
+                .ToList();
+
+            test.Types = types;
+
+            test.MinSuccessPercent = dto.MinimumSuccessPercent;
+
+
+            test.Questions.Clear();
+
+            foreach (var q in dto.Questions)
+            {
+                var question = new Question
+                {
+                    Text = q.Text,
+                    Options = q.Options.Select(o => new AnswerOption
+                    {
+                        Text = o.Text,
+                        IsCorrect = o.IsCorrect,
+                    }).ToList(),
+                    IsMultiple = q.isMultiple
+                };
+
+                test.Questions.Add(question);
+            }
+        }
+    }
+}
