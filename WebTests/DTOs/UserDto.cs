2ff63dfb76432696da781e126751745e7f4a37d0
neithiri <popokakapo76xxx@gmail.com>
2026-03-20 20:39:24 +0700

    Create UserDto.cs

---
+++ WebTests/DTOs/UserDto.cs
@@ -0,0 +1,13 @@
+﻿namespace WebTests.DTOs
+{
+    public class UserDto
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? AvatarUrl { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string? Status { get; set; }
+    }
+}
